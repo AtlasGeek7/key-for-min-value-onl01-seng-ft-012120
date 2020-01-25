1f53d3f618899a0cd37be41305ed92472f13fac5
Khalid T <khalid.tissire@gmail.com>
2020-01-25 02:39:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect { |k,v|  }
+  min = 1000000
+  name_hash.collect { |k,v|
+  if v <= min
+    
+  val = k 
+  }
 end
